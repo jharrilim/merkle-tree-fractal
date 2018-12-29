d5dd38650d2e9d51d7e05207a46803f4c85d4176
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-12-28 23:03:59 -0500

    Add comments

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export class FractalTree {
     private static merkleTreeLimit = 1 << 3;
 
     private merkleTree: MerkleTree;
+    private hash: any;
     private childTrees: FractalTree[];
     private childLimit: number;
     private treeState: AsyncIterableIterator<void>;
@@ -26,20 +27,41 @@ export class FractalTree {
         await this.treeState.next();
     }
 
+    /**
+     * Some random algorithm I made up to place hashes in a particular pattern.
+     * It first looks to fill the merkle tree until the limit is reached. After that,
+     * It will check the child limit to see if it can spawn children. If it cannot, it will
+     * just return. If it can, it will enter an endless cycle. It will create new children
+     * with size limits of half of what the parent has. Blah blah blah, it creates a binary
+     * tree in a way that I THINK is peculiar.
+     *
+     * @private
+     * @returns {AsyncIterableIterator<void>}
+     * @memberof FractalTree
+     */
     private async *_addNode(): AsyncIterableIterator<void> {
+        // First fill out this node's merkle tree
         while (this.merkleTree.length < FractalTree.merkleTreeLimit) {
             yield void await this.merkleTree.addNode(this.data);
         }
+        // Compute the hash and save it after it has finished adding all of the data to the tree
+        this.hash = await this.merkleTree.computeRootHash();
 
+        // this will bring you up to the parent
         if (this.childLimit === 0) {
             return;
         }
 
         while (true) {
+            // this.childLimit >> 1 so that you always start at the halfway point
             for (let i = this.childLimit >> 1; i < this.childLimit; ++i) {
+                // Each inner tree starts at half the size of it's parent
                 this.childTrees[i] = new FractalTree(this.childLimit >> 1);
+
+                // yields control into the child generator
                 yield* await this.childTrees[i]._addNode();
             }
+            // shift left and continue the cycle
             this.childLimit <<= 1;
         }
     }
