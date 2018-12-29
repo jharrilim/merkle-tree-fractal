cc7ff93944ea620474ff354b81eb7ba2bb893d30
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-12-28 22:03:17 -0500

    init

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { MerkleTree } from '@jharrilim/merkletree-js';
+
+
+export class FractalTree {
+    private static merkleTreeLimit = 1 << 3;
+
+    private merkleTree: MerkleTree;
+    private childTrees: FractalTree[];
+    private childLimit: number;
+    private treeState: AsyncIterableIterator<void>;
+    private data: any;
+
+
+    public constructor(maxChildren: number) {
+        this.merkleTree = MerkleTree.create();
+        this.childTrees = [];
+        this.childLimit = maxChildren;
+        this.treeState = {} as AsyncIterableIterator<void>;
+    }
+
+    public async addNode(data: any) {
+        this.data = data;
+        if (this.treeState === null) {
+            this.treeState = this._addNode();
+        }
+        await this.treeState.next();
+    }
+
+    private async *_addNode(): AsyncIterableIterator<void> {
+        while (this.merkleTree.length < FractalTree.merkleTreeLimit) {
+            yield void await this.merkleTree.addNode(this.data);
+        }
+
+        if (this.childLimit === 0) {
+            return;
+        }
+
+        while (true) {
+            for (let i = 0; i < this.childLimit; ++i) {
+                this.childTrees[i] = new FractalTree(this.childLimit >> 1);
+                yield* await this.childTrees[i]._addNode();
+            }
+            this.childLimit <<= 1;
+        }
+    }
+}
